e4bd1181ef32334b87380f7fb4d9c3664235c047
Hichanmin <pcm4106@naver.com>
2024-05-22 13:54:32 +0900

    2024.05.22(수) 수업

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
-import LandingPage from "./05_22_Study/LandingPage";
+// import './App.css';
+import NameForm from "./05_22_Study/NameForm";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <NameForm />
     </div>
   );
 }
